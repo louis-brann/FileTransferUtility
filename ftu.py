aaf1e6d5cb4589d1c1c384263bfbd7a3188de6a1
Guest User <Guest@apple.cs.hmc.edu>
2014-10-21 15:17:33 -0700

    Cleaned comments. Edited print statement at end of sending window to show total window progress, not individual window progress.

--- ftu.py
+++ ftu.py
@@ -50,8 +50,6 @@ def main(argv):
         currentWindow = 0
         numWindows = 2
         while currentWindow < numWindows:
-            print "currentWindow: " + str(currentWindow)
-            print "numWindows: " + str(numWindows)
 
             establishedTcp.setblocking(1)
 
@@ -60,7 +58,6 @@ def main(argv):
                 print "no data"
                 sys.exit(2)
             fileMetadata = pickle.loads(fileMetadataPickled)
-            print fileMetadata
 
             # We want future recv calls to be non-blocking
             establishedTcp.setblocking(0)
@@ -68,7 +65,6 @@ def main(argv):
         
             #Make data-structures to put data from udpSocket
             fileName, numPackets, numWindows = fileMetadata
-            print "numWindows: " + str(numWindows)
             fileBuffer = [None] * numPackets
             udpTimeoutCounter = 0
             while True:
@@ -78,7 +74,6 @@ def main(argv):
                 udpReady = select.select([udpSocket], [], [], .018)
 
                 if udpReady[0]:
-                    print "got udp!"
                     currentPacket, addr = udpSocket.recvfrom(packetSize)
 
                     # Put packet data into file buffer
@@ -90,8 +85,6 @@ def main(argv):
 
                     udpTimeoutCounter = 0
                     continue
-
-                    print "endof getting udp"
 
                 # If we've only timed out once or twice, start loop over
                 udpTimeoutCounter += 1
@@ -106,30 +99,8 @@ def main(argv):
 
                 #     #if we have received all data, break and close connections
                 if "0" not in missingPackets:
-                    print "no missing packets"
                     currentWindow += 1
                     break
-
-                # # Else, we are ready to check for done signal
-                # tcpReady = select.select([establishedTcp], [], [], .018)
-                # if tcpReady[0]:
-                #     data = establishedTcp.recv(packetSize)
-
-                #     #check which packets are missing
-                #     missingPackets = getMissingPackets(fileBuffer)
-
-                #     print "missing packets: " + missingPackets
-
-                    
-
-                #     #send this information to sender as bitset
-                #     establishedTcp.send(missingPackets)
-
-                #     #if we have received all data, break and close connections
-                #     if "0" not in missingPackets:
-                #         print "no missing packets"
-                #         currentWindow += 1
-                #         break
 
             # Close all connections
             # socket.shutdown(tcpSocket)
@@ -138,7 +109,6 @@ def main(argv):
             #socket.close(establishedTcp)
             #socket.close(udpSocket)
 
-            #outString = pickle.loads("".join(fileBuffer))
             outString = "".join(fileBuffer)
 
             if currentWindow - 1 == 0:
@@ -150,8 +120,6 @@ def main(argv):
 
     #Sender
     else:
-
-        fileName = "random.txt"
         #parse command line inputs
         if len(argv) == 2:
             source = argv[0]
@@ -176,11 +144,9 @@ def main(argv):
         inFile = open(fileName, 'r')
         fileSize = os.path.getsize(fileName)
         numWindows = int(math.ceil(float(fileSize) / float(windowSize)))
-        print "numWindow : " + str(numWindows)
 
         dataToSend = [None] * numWindows
         for i in range(numWindows):
-            print "out of while loop"
             dataToSend = inFile.read(windowSize)
 
             # Split window into packetsToSend
@@ -198,15 +164,13 @@ def main(argv):
             fileMetadataPickled = pickle.dumps(fileMetadata)
             tcpSocket.send(fileMetadataPickled)
 
-
             # Go through entire file, sending all packets until known to be transferred
             packetCounter = 0
             missingPackets = "0" * numPackets
             while missingPackets != "1" * numPackets:
-                print "in the inner while loop"
+
                 #UDP: send pickled data
                 if missingPackets[packetCounter] == "0":
-                    print "bout to send"
                     udpSocket.sendto(packetsToSend[packetCounter], (destIP,udpPort))
 
                 # Increment packet counter
@@ -214,16 +178,12 @@ def main(argv):
 
                 # If all packets are sent, send all done message
                 if packetCounter == numPackets:
-                    # tcpSocket.send("All done")
-                    # print "Sent all done!"
 
                     # Receive list of missed packets
                     missingPackets = tcpSocket.recv(packetSize)
-                    print "missingPackets: " + missingPackets
-                    print "checking against: " + numPackets * "1"
  
                     numMissing = missingPackets.count("0")
-                    print "%" + " done: " + str(1 - float(numMissing)/numPackets)
+                    print "%" + " done: " + str(float(i)/float(numWindows))
 
                     # Reset for next set of packets to send
                     packetCounter = 0
